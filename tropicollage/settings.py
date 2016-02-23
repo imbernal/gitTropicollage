91600bb6c78214e6b8a987eda091bea50c135db7
Israel Michel Bernal Martinez <imbernal9203@gmail.com>
2016-02-22 20:42:04 -0500

    Change Debug

--- tropicollage/settings.py
+++ tropicollage/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'arexbbsm5o@lvlgtmg)kplx4hf^etd#wm+gq=t376bgguxh@8x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['10.42.0.1', '127.0.0.1', '188.166.42.182']
 
